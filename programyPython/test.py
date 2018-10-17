08c96110d212c6789b58b6bf7518b04e1c7f439f
MrQlek <39280029+MrQlek@users.noreply.github.com>
2018-10-17 20:19:40 +0200

    Update programyPython/test.py

--- programyPython/test.py
+++ programyPython/test.py
@@ -1,6 +1,11 @@
 from Package import MotorControl
 from time import sleep
+import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(4, GPIO.OUT)
+GPIO.output(4, GPIO.LOW)
 #test = MotorControl(12,20,21,16)
 test = MotorControl(23,12,22,13)
 test.changeSpeed(50)
@@ -19,3 +24,5 @@ test.rotateInPoint(50,"R")
 sleep(2)
 test.rotateInPoint(50,"L")
 sleep(2)
+
+GPIO.cleanup()
