e7b895960005f0d4020bc4f03f027ac809ef4eab
MrQlek <39280029+MrQlek@users.noreply.github.com>
2018-11-08 00:20:04 +0100

    Add files via upload

--- programyPython/algorytmMapowania/Package/mapMatrixClass.py
+++ programyPython/algorytmMapowania/Package/mapMatrixClass.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 
 class mapMatrixClass():
-	def __init__(self,x,y):	
+	def __init__(self,x,y):
 		self.x = x
 		self.y = y
 		self.mapMatrix = np.zeros((x,y),dtype=np.byte)
@@ -61,7 +61,7 @@ class mapMatrixClass():
 		self.setSolidWall(4,4,"N")
 		self.setSolidWall(4,5,"S")
 		self.setSolidWall(4,6,"W")
-		self.setSolidWall(4,4,"E")
+		self.setSolidWall(4,6,"E")
 		self.setSolidWall(4,7,"N")
 		self.setSolidWall(4,8,"E")
 		self.setSolidWall(4,8,"S")
@@ -89,7 +89,7 @@ class mapMatrixClass():
 		self.setSolidWall(5,8,"W")
 		self.setSolidWall(5,9,"N")
 		self.setSolidWall(5,9,"W")
-		
+
 		self.setSolidWall(6,0,"S")
 		self.setSolidWall(6,1,"E")
 		self.setSolidWall(6,1,"W")
@@ -98,7 +98,7 @@ class mapMatrixClass():
 		self.setSolidWall(6,5,"W")
 		self.setSolidWall(6,6,"W")
 		self.setSolidWall(6,9,"N")
-		
+
 		self.setSolidWall(7,0,"S")
 		self.setSolidWall(7,0,"E")
 		self.setSolidWall(7,1,"E")
@@ -111,7 +111,7 @@ class mapMatrixClass():
 		self.setSolidWall(7,4,"S")
 		self.setSolidWall(7,7,"E")
 		self.setSolidWall(7,9,"N")
-		
+
 		self.setSolidWall(8,0,"S")
 		self.setSolidWall(8,0,"W")
 		self.setSolidWall(8,1,"W")
@@ -128,7 +128,7 @@ class mapMatrixClass():
 		self.setSolidWall(8,7,"W")
 		self.setSolidWall(8,8,"S")
 		self.setSolidWall(8,9,"N")
-		
+
 		self.setSolidWall(9,0,"S")
 		self.setSolidWall(9,0,"E")
 		self.setSolidWall(9,1,"E")
@@ -151,44 +151,320 @@ class mapMatrixClass():
 		self.setSolidWall(9,8,"E")
 		self.setSolidWall(9,9,"E")
 		self.setSolidWall(9,9,"N")
-		
+
 	def setSolidWall(self,x,y,direction):
 		if direction == "N" : self.mapMatrix[x][y] = self.mapMatrix[x][y] | 8
 		elif direction == "E" : self.mapMatrix[x][y] = self.mapMatrix[x][y] | 4
 		elif direction == "S" : self.mapMatrix[x][y] = self.mapMatrix[x][y] | 2
 		elif direction == "W" : self.mapMatrix[x][y] = self.mapMatrix[x][y] | 1
-		
+
 	def isWallExist(self,x,y,direction):
-		if direction == "N" : 
+		if direction == "N" :
 			if self.mapMatrix[x][y] & 8 == 8: return True
-		elif direction == "E" : 
+		elif direction == "E" :
 			if self.mapMatrix[x][y] & 4 == 4: return True
-		elif direction == "S" : 
+		elif direction == "S" :
 			if self.mapMatrix[x][y] & 2 == 2: return True
-		elif direction == "W" : 
+		elif direction == "W" :
 			if self.mapMatrix[x][y] & 1 == 1: return True
 		return False
 
 	def setVisited(self,x,y):
 		self.mapMatrix[x][y] = self.mapMatrix[x][y] | 16
-	
-	def wasVisited(self,x,y):		#Nie sprawdzona
+
+	def wasVisited(self,x,y):
 		if self.mapMatrix[x][y] & 16 == 16: return True
 		else: return False
-		
+
 	def setNotAvailable(self,x,y):
 		self.mapMatrix[x][y] = self.mapMatrix[x][y] | 32
-	
-	def isNotAvailable(self,x,y):	#Nie sprawdzona
+
+	def isNotAvailable(self,x,y):
 		if self.mapMatrix[x][y] & 32 == 32: return True
 		else: return False
-		
+
+	def findWayToNearestNoVisitedSpot(self,robotX,robotY):
+		pathMap = np.zeros((10,10),dtype=np.byte)
+		pathMap[robotX][robotY]=1
+
+		actualValue=1
+		findX=0
+		findY=0
+		endLoop=0
+
+		while (endLoop==0):
+			changeAmount=0
+			for i in range(0,10):
+				for j in range(0,10):
+					if(pathMap[i][j] == actualValue):
+						if(not self.isWallExist(i,j,"W")):
+							if(pathMap[i-1][j]==0):
+								pathMap[i-1][j]=actualValue+1
+								changeAmount=changeAmount+1
+								if(not self.wasVisited(i-1,j)):
+									findX=i-1
+									findY=j
+									endLoop=1
+									break
+						if(not self.isWallExist(i,j,"N")):
+							if(pathMap[i][j+1]==0):
+								pathMap[i][j+1]=actualValue+1
+								changeAmount=changeAmount+1
+								if(not self.wasVisited(i,j+1)):
+									findX=i
+									findY=j+1
+									endLoop=1
+									break
+						if(not self.isWallExist(i,j,"E")):
+							if(pathMap[i+1][j]==0):
+								pathMap[i+1][j]=actualValue+1
+								changeAmount=changeAmount+1
+								if(not self.wasVisited(i+1,j)):
+									findX=i+1
+									findY=j
+									endLoop=1
+									break
+						if(not self.isWallExist(i,j,"S")):
+							if(pathMap[i][j-1]==0):
+								pathMap[i][j-1]=actualValue+1
+								changeAmount=changeAmount+1
+								if(not self.wasVisited(i,j-1)):
+									findX=i
+									findY=j-1
+									endLoop=1
+									break
+				if(endLoop!=0):break
+			actualValue = actualValue + 1
+			if(changeAmount==0):
+				pathMap[0][0]=-1
+				return pathMap
+
+
+		#wyznacz trase do znalecionego punktu
+		backPathMap = np.zeros((10,10),dtype=np.byte)
+		newValue = 1
+		while (pathMap[findX][findY] != 1):
+			backPathMap[findX][findY] = newValue
+			if(findX != 9 and pathMap[findX+1][findY] == actualValue-1):
+				findX = findX+1
+				actualValue = actualValue-1
+			elif(findX !=0  and pathMap[findX-1][findY] == actualValue-1):
+				findX = findX-1
+				actualValue = actualValue-1
+			elif(findY != 9 and pathMap[findX][findY+1] == actualValue-1):
+				findY = findY+1
+				actualValue = actualValue-1
+			elif(findY != 0 and pathMap[findX][findY-1] == actualValue-1):
+				findY = findY-1
+				actualValue = actualValue-1
+
+			newValue = newValue+1
+
+		backPathMap[findX][findY] = newValue
+		#print backPathMap
+		return backPathMap
+
+	def findWayToNearestNoVisitedSpot2(self,robotX,robotY,course):
+		pathMap = np.zeros((10,10),dtype=np.byte)
+		pathMap[robotX][robotY]=1
+
+		actualValue=1
+		findX=0
+		findY=0
+		endLoop=0
+
+		while (endLoop==0):
+			changeAmount=0
+			for i in range(0,10):
+				for j in range(0,10):
+					if(pathMap[i][j] == actualValue):
+						if course == 0:
+							if(not self.isWallExist(i,j,"W")):
+								if(pathMap[i-1][j]==0):
+									pathMap[i-1][j]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i-1,j)):
+										findX=i-1
+										findY=j
+										endLoop=1
+										break
+							if(not self.isWallExist(i,j,"N")):
+								if(pathMap[i][j+1]==0):
+									pathMap[i][j+1]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i,j+1)):
+										findX=i
+										findY=j+1
+										endLoop=1
+										break
+							if(not self.isWallExist(i,j,"E")):
+								if(pathMap[i+1][j]==0):
+									pathMap[i+1][j]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i+1,j)):
+										findX=i+1
+										findY=j
+										endLoop=1
+										break
+							if(not self.isWallExist(i,j,"S")):
+								if(pathMap[i][j-1]==0):
+									pathMap[i][j-1]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i,j-1)):
+										findX=i
+										findY=j-1
+										endLoop=1
+										break
+						elif course == 90:
+							if(not self.isWallExist(i,j,"N")):
+								if(pathMap[i][j+1]==0):
+									pathMap[i][j+1]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i,j+1)):
+										findX=i
+										findY=j+1
+										endLoop=1
+										break
+							if(not self.isWallExist(i,j,"E")):
+								if(pathMap[i+1][j]==0):
+									pathMap[i+1][j]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i+1,j)):
+										findX=i+1
+										findY=j
+										endLoop=1
+										break
+							if(not self.isWallExist(i,j,"S")):
+								if(pathMap[i][j-1]==0):
+									pathMap[i][j-1]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i,j-1)):
+										findX=i
+										findY=j-1
+										endLoop=1
+										break
+							if(not self.isWallExist(i,j,"W")):
+								if(pathMap[i-1][j]==0):
+									pathMap[i-1][j]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i-1,j)):
+										findX=i-1
+										findY=j
+										endLoop=1
+										break
+						elif course == 180:
+							if(not self.isWallExist(i,j,"E")):
+								if(pathMap[i+1][j]==0):
+									pathMap[i+1][j]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i+1,j)):
+										findX=i+1
+										findY=j
+										endLoop=1
+										break
+							if(not self.isWallExist(i,j,"S")):
+								if(pathMap[i][j-1]==0):
+									pathMap[i][j-1]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i,j-1)):
+										findX=i
+										findY=j-1
+										endLoop=1
+										break
+							if(not self.isWallExist(i,j,"W")):
+								if(pathMap[i-1][j]==0):
+									pathMap[i-1][j]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i-1,j)):
+										findX=i-1
+										findY=j
+										endLoop=1
+										break
+							if(not self.isWallExist(i,j,"N")):
+								if(pathMap[i][j+1]==0):
+									pathMap[i][j+1]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i,j+1)):
+										findX=i
+										findY=j+1
+										endLoop=1
+										break
+						elif course ==270:
+							if(not self.isWallExist(i,j,"S")):
+								if(pathMap[i][j-1]==0):
+									pathMap[i][j-1]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i,j-1)):
+										findX=i
+										findY=j-1
+										endLoop=1
+										break
+							if(not self.isWallExist(i,j,"W")):
+								if(pathMap[i-1][j]==0):
+									pathMap[i-1][j]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i-1,j)):
+										findX=i-1
+										findY=j
+										endLoop=1
+										break
+							if(not self.isWallExist(i,j,"N")):
+								if(pathMap[i][j+1]==0):
+									pathMap[i][j+1]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i,j+1)):
+										findX=i
+										findY=j+1
+										endLoop=1
+										break
+							if(not self.isWallExist(i,j,"E")):
+								if(pathMap[i+1][j]==0):
+									pathMap[i+1][j]=actualValue+1
+									changeAmount=changeAmount+1
+									if(not self.wasVisited(i+1,j)):
+										findX=i+1
+										findY=j
+										endLoop=1
+										break
+
+
+				if(endLoop!=0):break
+			actualValue = actualValue + 1
+			if(changeAmount==0):
+				pathMap[0][0]=-1
+				return pathMap
+
+
+		#wyznacz trase do znalecionego punktu
+		backPathMap = np.zeros((10,10),dtype=np.byte)
+		newValue = 1
+		while (pathMap[findX][findY] != 1):
+			backPathMap[findX][findY] = newValue
+			if(findX != 9 and pathMap[findX+1][findY] == actualValue-1):
+				findX = findX+1
+				actualValue = actualValue-1
+			elif(findX !=0  and pathMap[findX-1][findY] == actualValue-1):
+				findX = findX-1
+				actualValue = actualValue-1
+			elif(findY != 9 and pathMap[findX][findY+1] == actualValue-1):
+				findY = findY+1
+				actualValue = actualValue-1
+			elif(findY != 0 and pathMap[findX][findY-1] == actualValue-1):
+				findY = findY-1
+				actualValue = actualValue-1
+
+			newValue = newValue+1
+
+		backPathMap[findX][findY] = newValue
+		#print backPathMap
+		return backPathMap
+
 	def drawMatrix(self):
 		sys.stdout.flush()
 		for k in range(0,self.x):
 			sys.stdout.write('____')
 		print("")
-		for i in range(self.y-1,-1,-1): 
+		for i in range(self.y-1,-1,-1):
 			for k in range(0,2):
 				for j in range(0,self.x):
 					if(j is not 0):
@@ -208,21 +484,3 @@ class mapMatrixClass():
 						else: sys.stdout.write("   ")
 					if(j==self.x-1):sys.stdout.write("|")
 				print("")
-				
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- programyPython/algorytmMapowania/Package/robotClass.py
+++ programyPython/algorytmMapowania/Package/robotClass.py
@@ -1,5 +1,5 @@
 import numpy as np
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
 #from Package import MotorControl
 from mapMatrixClass import mapMatrixClass
@@ -7,31 +7,27 @@ from mapMatrixClass import mapMatrixClass
 class robotClass:
 	def __init__(self):
 		#GPIO.setmode(GPIO.BCM)
-		
+
 		#inicjalizacja silnikow
 		#motors = MotorControl(23,12,22,13)
-		
+
 		#inicjalizacja sensorow
 		#/########################
-		
+
 		#wykorzystwane tylko do testowania algorytmu potem do usuniecia
-		self.simulationMap = mapMatrixClass(10,10)		
+		self.simulationMap = mapMatrixClass(10,10)
 		self.simulationMap.prepareTable()
 		self.course = 0
-		self.setRobotPosition()
 		#okreslenie pola poczatkowego
+		self.moves=0
 		self.startX = 0
 		self.startY = 0
-		
+
 		self.x = self.startX
 		self.y = self.startY
 		#inicjalizacja mapy
 		self.map = mapMatrixClass(10,10)
-	
-	def setRobotPosition(self):
-		return True
-		#ustawic tak robota zeby jego lewy czujnik dotykal krawedz pomieszczenia i tak zmien self.course zeby zgadzalo sie z polozeniem robota
-		
+
 	def setCourse(self, angle):  #dodaje do obecnego kierunku zadana wartosc
 		if ((angle == 90) or (angle == 180) or (angle == 270)):
 			course = self.course + angle
@@ -39,23 +35,23 @@ class robotClass:
 			elif course == 360: self.course = 0
 			elif course == 450: self.course = 90
 			elif course == 540: self.course = 180
-		
+
 	def checkSensor(self):
 		#do testow, pozniej do zamienia przez odczyty z czujnikow ktory bedzie trzeba uzaleznic od wartosci self.course
-		if self.simulationMap.isWallExist(self.x,self.y,"N"): 
+		if self.simulationMap.isWallExist(self.x,self.y,"N"):
 			self.map.setSolidWall(self.x,self.y,"N")
 			if self.y !=9 : self.map.setSolidWall(self.x,self.y+1,"S")
-		if self.simulationMap.isWallExist(self.x,self.y,"E"): 
+		if self.simulationMap.isWallExist(self.x,self.y,"E"):
 			self.map.setSolidWall(self.x,self.y,"E")
 			if self.x !=9 : self.map.setSolidWall(self.x+1,self.y,"W")
-		if self.simulationMap.isWallExist(self.x,self.y,"S"): 
+		if self.simulationMap.isWallExist(self.x,self.y,"S"):
 			self.map.setSolidWall(self.x,self.y,"S")
 			if self.y !=0 : self.map.setSolidWall(self.x,self.y-1,"N")
-		if self.simulationMap.isWallExist(self.x,self.y,"W"): 
+		if self.simulationMap.isWallExist(self.x,self.y,"W"):
 			self.map.setSolidWall(self.x,self.y,"W")
 			if self.x !=0 : self.map.setSolidWall(self.x-1,self.y,"E")
 		self.map.setVisited(self.x,self.y)
-		
+
 	def go(self,direction = "F"):
 		if direction == "F":
 			#we wlaciwym kodzie tylko porusz sie za pomoca silnikow o jedno pole course
@@ -69,14 +65,55 @@ class robotClass:
 		else:
 			#we wlasiciwym kodzie obroc sie w lewo i porusz o jedno pole zmien wartosc course o +270
 			self.setCourse(270)
-			
+
 		if self.course == 0: self.y = self.y + 1
 		elif self.course == 90: self.x = self.x + 1
 		elif self.course == 180: self.y = self.y - 1
 		elif self.course == 270: self.x = self.x - 1
-	
-	
-	
-	
-	
-	
+
+	def goTo(self,backPathMap):
+		while (backPathMap[self.x][self.y] != 1):
+			actualValue = backPathMap[self.x][self.y]
+
+			if self.course == 0:
+				if(self.x != 9 and backPathMap[self.x+1][self.y] == actualValue-1):
+					self.go("R")
+				elif(self.x != 0 and backPathMap[self.x-1][self.y] == actualValue-1):
+					self.go("L")
+				elif(self.y != 9 and backPathMap[self.x][self.y+1] == actualValue-1):
+					self.go()
+				elif(self.y != 0 and backPathMap[self.x][self.y-1] == actualValue-1):
+					self.go("B")
+
+
+			elif self.course == 90:
+				if(self.x != 9 and backPathMap[self.x+1][self.y] == actualValue-1):
+					self.go()
+				elif(self.x != 0 and backPathMap[self.x-1][self.y] == actualValue-1):
+					self.go("B")
+				elif(self.y != 9 and backPathMap[self.x][self.y+1] == actualValue-1):
+					self.go("L")
+				elif(self.y != 0 and backPathMap[self.x][self.y-1] == actualValue-1):
+					self.go("R")
+
+			elif self.course == 180:
+				if(self.x != 9 and backPathMap[self.x+1][self.y] == actualValue-1):
+					self.go("L")
+				elif(self.x != 0 and backPathMap[self.x-1][self.y] == actualValue-1):
+					self.go("R")
+				elif(self.y != 9 and backPathMap[self.x][self.y+1] == actualValue-1):
+					self.go("B")
+				elif(self.y != 0 and backPathMap[self.x][self.y-1] == actualValue-1):
+					self.go()
+
+			elif self.course == 270:
+				if(self.x != 9 and backPathMap[self.x+1][self.y] == actualValue-1):
+					self.go("B")
+				elif(self.x != 0 and backPathMap[self.x-1][self.y] == actualValue-1):
+					self.go()
+				elif(self.y != 9 and backPathMap[self.x][self.y+1] == actualValue-1):
+					self.go("R")
+				elif(self.y != 0 and backPathMap[self.x][self.y-1] == actualValue-1):
+					self.go("L")
+
+			self.moves=self.moves+1
