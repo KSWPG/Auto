e0f800d651a6019f2cd34176612f181c27229a4d
MrQlek <39280029+MrQlek@users.noreply.github.com>
2018-11-08 00:19:29 +0100

    Add files via upload

--- programyPython/algorytmMapowania/mappingAlgorithm2.py
+++ programyPython/algorytmMapowania/mappingAlgorithm2.py
@@ -1,128 +1,74 @@
 from Package import robotClass
 import numpy as np
 
-class mappingAlgorithm2:
-	def __init__(self):
-		print("Rozpoczecie procesu mapowania")
-		self.robot = robotClass()
-		#self.robot.checkSensor()
-		#self.findNearestNoVisitedSpot()
-		self.moves=0
-		while True:
-			self.robot.checkSensor()
-			backPathMap = self.findNearestNoVisitedSpot()
-			if(backPathMap[0][0] == -1): break
-			
-			self.goTo(backPathMap)
-			
-		print("Zakonczono proces mapowania w %i ruchach"%self.moves)
-		self.robot.map.drawMatrix()
-		
-	def findNearestNoVisitedSpot(self):
-		pathMap = np.zeros((10,10),dtype=np.byte)
-		pathMap[self.robot.x][self.robot.y]=1
-		actualValue=1
-		findX=0
-		findY=0
-		endLoop=0
-		#znajdz najblizszy nieodwiedzony punkt
-		while (endLoop==0):
-			changeAmount=0
-			for i in range(0,10): 
-				for j in range(0,10):
-					if(pathMap[i][j] == actualValue):
-						if(not self.robot.map.isWallExist(i,j,"W")):
-							if(pathMap[i-1][j]==0):
-								pathMap[i-1][j]=actualValue+1
-								changeAmount=changeAmount+1
-								if(not self.robot.map.wasVisited(i-1,j)): findX=i-1;findY=j;endLoop=1;break
-						if(not self.robot.map.isWallExist(i,j,"N")):
-							if(pathMap[i][j+1]==0):
-								pathMap[i][j+1]=actualValue+1
-								changeAmount=changeAmount+1
-								if(not self.robot.map.wasVisited(i,j+1)): findX=i;findY=j+1;endLoop=1;break
-						if(not self.robot.map.isWallExist(i,j,"E")):
-							if(pathMap[i+1][j]==0):
-								pathMap[i+1][j]=actualValue+1
-								changeAmount=changeAmount+1
-								if(not self.robot.map.wasVisited(i+1,j)): findX=i+1;findY=j;endLoop=1;break
-						if(not self.robot.map.isWallExist(i,j,"S")):
-							if(pathMap[i][j-1]==0):
-								pathMap[i][j-1]=actualValue+1
-								changeAmount=changeAmount+1
-								if(not self.robot.map.wasVisited(i,j-1)): findX=i;findY=j-1;endLoop=1;break
-					
-						
-					
-					if(endLoop!=0):break
-			actualValue = actualValue + 1
-			if(changeAmount==0): 
-				pathMap[0][0]=-1
-				return pathMap  #zakonczono mapowanie 
-		
-		#print pathMap
-		#wyznacz trase do znalecionego punktu
-		backPathMap = np.zeros((10,10),dtype=np.byte)
-		newValue = 1
-		while (pathMap[findX][findY] != 1):
-			backPathMap[findX][findY] = newValue
-			if(findX != 9 and pathMap[findX+1][findY] == actualValue-1): findX = findX+1;actualValue = actualValue-1
-			elif(findX !=0  and pathMap[findX-1][findY] == actualValue-1): findX = findX-1;actualValue = actualValue-1
-			elif(findY != 9 and pathMap[findX][findY+1] == actualValue-1): findY = findY+1;actualValue = actualValue-1
-			elif(findY != 0 and pathMap[findX][findY-1] == actualValue-1): findY = findY-1;actualValue = actualValue-1
-			
-			newValue = newValue+1
+def mappingAlgorithm2(robot):
+	#print("Rozpoczecie procesu mapowania")
+	while True:
+		robot.checkSensor()
+		backPathMap = robot.map.findWayToNearestNoVisitedSpot(robot.x,robot.y)
+		if(backPathMap[0][0] == -1): break
 
-		backPathMap[findX][findY] = newValue
-		#print backPathMap
-		return backPathMap
-		
-	def goTo(self,backPathMap):
-		while (backPathMap[self.robot.x][self.robot.y] != 1):
-			actualValue = backPathMap[self.robot.x][self.robot.y]
-			
-			if self.robot.course == 0:
-				if(self.robot.x != 9 and backPathMap[self.robot.x+1][self.robot.y] == actualValue-1):
-					self.robot.go("R")
-				elif(self.robot.x != 0 and backPathMap[self.robot.x-1][self.robot.y] == actualValue-1):
-					self.robot.go("L")
-				elif(self.robot.y != 9 and backPathMap[self.robot.x][self.robot.y+1] == actualValue-1):
-					self.robot.go()
-				elif(self.robot.y != 0 and backPathMap[self.robot.x][self.robot.y-1] == actualValue-1):
-					self.robot.go("B")
+		robot.goTo(backPathMap)
 
-						
-			elif self.robot.course == 90:
-				if(self.robot.x != 9 and backPathMap[self.robot.x+1][self.robot.y] == actualValue-1):
-					self.robot.go()
-				elif(self.robot.x != 0 and backPathMap[self.robot.x-1][self.robot.y] == actualValue-1):
-					self.robot.go("B")
-				elif(self.robot.y != 9 and backPathMap[self.robot.x][self.robot.y+1] == actualValue-1):
-					self.robot.go("L")
-				elif(self.robot.y != 0 and backPathMap[self.robot.x][self.robot.y-1] == actualValue-1):
-					self.robot.go("R")
-						
-			elif self.robot.course == 180:
-				if(self.robot.x != 9 and backPathMap[self.robot.x+1][self.robot.y] == actualValue-1):
-					self.robot.go("L")
-				elif(self.robot.x != 0 and backPathMap[self.robot.x-1][self.robot.y] == actualValue-1):
-					self.robot.go("R")
-				elif(self.robot.y != 9 and backPathMap[self.robot.x][self.robot.y+1] == actualValue-1):
-					self.robot.go("B")
-				elif(self.robot.y != 0 and backPathMap[self.robot.x][self.robot.y-1] == actualValue-1):
-					self.robot.go()
-						
-			elif self.robot.course == 270:
-				if(self.robot.x != 9 and backPathMap[self.robot.x+1][self.robot.y] == actualValue-1):
-					self.robot.go("B")
-				elif(self.robot.x != 0 and backPathMap[self.robot.x-1][self.robot.y] == actualValue-1):
-					self.robot.go()
-				elif(self.robot.y != 9 and backPathMap[self.robot.x][self.robot.y+1] == actualValue-1):
-					self.robot.go("R")
-				elif(self.robot.y != 0 and backPathMap[self.robot.x][self.robot.y-1] == actualValue-1):
-					self.robot.go("L")
-		
-			self.moves=self.moves+1
+	#print("Zakonczono proces mapowania w %i ruchach" % robot.moves)
+	#robot.map.drawMatrix()
+	return robot.moves
 
-		
-mappingAlgorithm2()	
+def mappingAlgorithm3(robot):
+	#print("Rozpoczecie procesu mapowania")
+	while True:
+		robot.checkSensor()
+		backPathMap = robot.map.findWayToNearestNoVisitedSpot2(robot.x,robot.y,robot.course)
+		if(backPathMap[0][0] == -1): break
+
+		robot.goTo(backPathMap)
+
+	#print("Zakonczono proces mapowania w %i ruchach" % robot.moves)
+	#robot.map.drawMatrix()
+
+
+def testAlgorithmsHowManyMovesIsNeeded():
+	better2 = 0
+	better3 = 0
+	equal = 0
+	for i in range(0,10):
+		for j in range(0,10):
+			for k in [0,90,180,270]:
+				robot = robotClass()
+				robot.x = i
+				robot.y = j
+				mappingAlgorithm2(robot)
+				algorithm2Moves = robot.moves
+				robot2 = robotClass()
+				robot.x = i
+				robot.y = j
+				robot.course = k
+				mappingAlgorithm3(robot)
+				algorithm3Moves = robot.moves
+
+				if (algorithm2Moves < algorithm3Moves): better2 = better2 + 1
+				elif (algorithm2Moves > algorithm3Moves): better3 = better3 + 1
+				else: equal = equal + 1
+
+	print("mappingAlgorithm2 byl lepszy %i razy" % better2)
+	print("mappingAlgorithm3 byl lepszy %i razy" % better3)
+	print("algorytmy byly sobie rowne %i razy" % equal)
+
+def showHowManyMovesIsNeededToMap():
+	matrix = np.zeros((100),dtype=np.byte)
+	matrixNumber = 0
+	for i in range(0,10):
+		for j in range(0,10):
+			robot = robotClass()
+			robot.x = i
+			robot.y = j
+			mappingAlgorithm2(robot)
+			matrix[matrixNumber] = robot.moves
+
+			matrix.sort()
+
+	for i in range(0,100):
+		print(matrix[i])
+
+
+showHowManyMovesIsNeededToMap()
