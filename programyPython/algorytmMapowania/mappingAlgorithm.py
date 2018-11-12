7f79921e157cf6626460b88f09b60da8d881e9c0
MrQlek <39280029+MrQlek@users.noreply.github.com>
2018-11-12 22:51:00 +0100

    Usunięcie starego algorytmu
    Uznałem że nie ma sensu już trzymać tego algorytmu od którego zaczynałem więc zamieniam go na aktualne, działające wersje.

--- programyPython/algorytmMapowania/mappingAlgorithm.py
+++ programyPython/algorytmMapowania/mappingAlgorithm.py
@@ -1,70 +1,106 @@
 from Package import robotClass
+from Package import simulationMapMatrixClass
 
-class mappingAlgorithm:
-	def __init__(self):
-		print("Rozpoczecie procesu mapowania")
-		self.robot = robotClass()
-		if(self.checkEdge()):
-			print("Zmapowano krawedz obszaru")
-			self.robot.map.drawMatrix()
-			#dalsza czesc programu
+import time
 
-	def checkEdge(self):
-		while True:
-			self.robot.checkSensor()
-			if self.robot.course == 0:
-				if(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"W")):
-					self.robot.go("L")
-				elif(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"N")):
-					self.robot.go()
-				elif(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"E")):
-					self.robot.go("R")
-				elif(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"S")):
-					self.robot.go("B")
-				else:
-					print("Robot nie moze wykonac ruchu")
-					return False
-					
-			elif self.robot.course == 90:
-				if(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"N")):
-					self.robot.go("L")
-				elif(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"E")):
-					self.robot.go()
-				elif(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"S")):
-					self.robot.go("R")
-				elif(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"W")):
-					self.robot.go("B")
-				else:
-					print("Robot nie moze wykonac ruchu")
-					return False
-					
-			elif self.robot.course == 180:
-				if(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"E")):
-					self.robot.go("L")
-				elif(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"S")):
-					self.robot.go()
-				elif(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"W")):
-					self.robot.go("R")
-				elif(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"N")):
-					self.robot.go("B")
-				else:
-					print("Robot nie moze wykonac ruchu")
-					return False
-					
-			elif self.robot.course == 270:
-				if(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"S")):
-					self.robot.go("L")
-				elif(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"W")):
-					self.robot.go()
-				elif(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"N")):
-					self.robot.go("R")
-				elif(not self.robot.map.isWallExist(self.robot.x,self.robot.y,"E")):
-					self.robot.go("B")
-				else:
-					print("Robot nie moze wykonac ruchu")
-					return False
-			if(self.robot.x == self.robot.startX and self.robot.y == self.robot.startY): 
-				return True
-		
+import numpy as np
 
-mappingAlgorithm()
+def mappingAlgorithm1(robot):
+	while True:
+		robot.checkSensor()
+		try:
+			PathMap = robot.map.findWayToNearestNoVisitedSpot(robot.x,robot.y)
+			robot.goTo(PathMap)
+		except Exception as e:
+			if(str(e) == "Not found anymore no visited spot"):
+				break
+
+
+
+	#print("Zakonczono proces mapowania w %i ruchach" % robot.moves)
+	#robot.map.drawMatrix()
+
+def mappingAlgorithm2(robot):
+	while True:
+		robot.checkSensor()
+		try:
+			PathMap = robot.map.findWayToNearestNoVisitedSpot2(robot.x,robot.y,robot.course)
+			robot.goTo(PathMap)
+		except Exception as e:
+			if(str(e) == "Not found anymore no visited spot"):
+				break
+
+	#print("Zakonczono proces mapowania w %i ruchach" % robot.moves)
+	#robot.map.drawMatrix()
+
+
+def testAlgorithmsHowManyMovesIsNeeded():
+	better1 = 0
+	better2 = 0
+	theBestMappingAlgorithm1Time= 0
+	theBestMappingAlgorithm2Time = 0
+	equal = 0
+	for z in range(0,10):
+		simulationMap = simulationMapMatrixClass(10,10)
+		simulationMap.generateRandomMap()
+		for i in range(0,simulationMap.xSize):
+			for j in range(0,simulationMap.ySize):
+				for k in [0,90,180,270]:
+					robot = robotClass()
+					robot.x = i
+					robot.y = j
+					robot.simulationMap=simulationMap
+
+					start_time = time.time()
+					mappingAlgorithm1(robot)
+					mappingAlgorithm1Time = time.time()-start_time
+					algorithm1Moves = robot.moves
+
+					robot2 = robotClass()
+					robot2.x = i
+					robot2.y = j
+					robot2.course = k
+					robot2.simulationMap = simulationMap
+
+					start_time = time.time()
+					mappingAlgorithm2(robot2)
+					mappingAlgorithm2Time = time.time()-start_time
+					algorithm2Moves = robot2.moves
+
+					timeDifferecne = mappingAlgorithm2Time - mappingAlgorithm1Time
+					if (timeDifferecne > theBestMappingAlgorithm1Time):
+						theBestMappingAlgorithm1Time = timeDifferecne
+					elif(timeDifferecne < theBestMappingAlgorithm2Time):
+						theBestMappingAlgorithm2Time = timeDifferecne
+
+					if (algorithm1Moves < algorithm2Moves): better1 = better1 + 1
+					elif (algorithm1Moves > algorithm2Moves): better2 = better2 + 1
+					else: equal = equal + 1
+		print(z)
+
+	print("mappingAlgorithm1 byl lepszy %i razy" % better1)
+	print("mappingAlgorithm2 byl lepszy %i razy" % better2)
+	print("algorytmy byly sobie rowne %i razy" % equal)
+
+	print("W najlepszym wypadku mappingAlgorithm1 byl szybszy o %f" % theBestMappingAlgorithm1Time)
+	print("W najlepszym wypadku mappingAlgorithm2 byl szybszy o %f" % (theBestMappingAlgorithm2Time*(-1)))
+
+
+def showHowManyMovesIsNeededToMap():
+	matrix = np.zeros((100),dtype=np.byte)
+	matrixNumber = 0
+	for i in range(0,10):
+		for j in range(0,10):
+			robot = robotClass()
+			robot.x = i
+			robot.y = j
+			mappingAlgorithm1(robot)
+			matrix[matrixNumber] = robot.moves
+
+			matrix.sort()
+
+	for i in range(0,100):
+		print(matrix[i])
+
+
+testAlgorithmsHowManyMovesIsNeeded()
