6ff8591e9ad9ef013ce5b0ea496958c73c2d21df
MrQlek <39280029+MrQlek@users.noreply.github.com>
2018-11-17 13:04:41 +0100

    Dodanie koljenego testu algorytmow

--- programyPython/algorytmMapowania/mappingAlgorithm.py
+++ programyPython/algorytmMapowania/mappingAlgorithm.py
@@ -39,13 +39,18 @@ def mappingAlgorithm2(robot):
 def testAlgorithmsHowManyMovesIsNeeded():
 	better1 = 0
 	better2 = 0
+
 	theBestMappingAlgorithm1Time= 0
 	theBestMappingAlgorithm2Time = 0
+
+	MappingAlgorithm1WasFaster = 0
+	MappingAlgorithm2WasFaster = 0
+	TimeWasEqual = 0
+
 	equal = 0
-	for z in range(0,10):
+	for z in range(0,100):
 		simulationMap = simulationMapMatrixClass(10,10)
 		simulationMap.generateRandomMap()
-		simulationMap.drawMap()
 		robot = robotClass()
 		robot.simulationMap=simulationMap
 
@@ -63,23 +68,37 @@ def testAlgorithmsHowManyMovesIsNeeded():
 		algorithm2Moves = robot2.moves
 
 		timeDifferecne = mappingAlgorithm2Time - mappingAlgorithm1Time
+
+		if(timeDifferecne>0):
+			MappingAlgorithm1WasFaster = MappingAlgorithm1WasFaster +1
+		elif(timeDifferecne<0):
+			MappingAlgorithm2WasFaster = MappingAlgorithm2WasFaster +1
+		else:
+			TimeWasEqual = TimeWasEqual +1
+
 		if (timeDifferecne > theBestMappingAlgorithm1Time):
 			theBestMappingAlgorithm1Time = timeDifferecne
 		elif(timeDifferecne < theBestMappingAlgorithm2Time):
 			theBestMappingAlgorithm2Time = timeDifferecne
 
-		if (algorithm1Moves < algorithm2Moves): better1 = better1 + 1
-		elif (algorithm1Moves > algorithm2Moves): better2 = better2 + 1
-		else: equal = equal + 1
+		if (algorithm1Moves < algorithm2Moves):
+			 better1 = better1 + 1
+		elif (algorithm1Moves > algorithm2Moves):
+			better2 = better2 + 1
+		else:
+			equal = equal + 1
 		print(z)
 
-	print("mappingAlgorithm1 byl lepszy %i razy" % better1)
-	print("mappingAlgorithm2 byl lepszy %i razy" % better2)
-	print("algorytmy byly sobie rowne %i razy" % equal)
+	print("mappingAlgorithm1 wykonal mniej ruchow %i razy" % better1)
+	print("mappingAlgorithm2 wykonal mniej ruchow %i razy" % better2)
+	print("algorytmy wykonaly tyle samo ruchow %i razy" % equal)
 
 	print("W najlepszym wypadku mappingAlgorithm1 byl szybszy o %f" % theBestMappingAlgorithm1Time)
 	print("W najlepszym wypadku mappingAlgorithm2 byl szybszy o %f" % (theBestMappingAlgorithm2Time*(-1)))
 
+	print("mappingAlgorithm1 byl szybszy %i razy" % MappingAlgorithm1WasFaster)
+	print("mappingAlgorithm2 byl szybszy %i razy" % MappingAlgorithm2WasFaster)
+	print("Algorytmy byly tak samo szybkie %i razy" % TimeWasEqual)
 
 def showHowManyMovesIsNeededToMap():
 	matrix = np.zeros((100),dtype=np.byte)
@@ -111,4 +130,4 @@ def mappingTest():
 	mappingAlgorithm1(robot)
 	robot.map.drawMap()
 
-mappingTest()
+testAlgorithmsHowManyMovesIsNeeded()
